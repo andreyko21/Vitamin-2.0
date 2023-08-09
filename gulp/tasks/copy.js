82dbc8ca9063c7fad313465e616b74a2805a6cf3
andreyko21 <andriy.zakharkiv.2001@gmail.com>
2023-08-09 16:00:50 +0300

    Initial dist subtree commit

--- gulp/tasks/copy.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp';
-import { filePaths } from '../config/paths.js';
-
-const copy = () => {
-  return gulp.src(filePaths.src.static).pipe(gulp.dest(filePaths.build.static));
-};
-
-export { copy };
