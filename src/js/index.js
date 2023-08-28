975662a759808583e747043698b0d29e0efe4d18
andreyko21 <andriy.zakharkiv.2001@gmail.com>
2023-08-28 18:54:47 +0300

    fix home page

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,50 @@
 import BurgerMenu from './modules/BurgerMenu';
-
+import Swiper from 'swiper';
 new BurgerMenu().init();
+
+document.addEventListener('DOMContentLoaded', function () {
+  const swiperContainer = document.querySelector('.swiper-container');
+  if (swiperContainer) {
+    const mySwiper = new Swiper(swiperContainer, {
+      slidesPerView: 1,
+      spaceBetween: 32,
+      breakpoints: {
+        600: {
+          slidesPerView: 2,
+        },
+        960: {
+          slidesPerView: 3,
+        },
+      },
+    });
+    
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const swiperContainer = document.querySelector('.products-swiper-container');
+  if (swiperContainer) {
+    const mySwiper = new Swiper(swiperContainer, {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      breakpoints: {
+        401: {
+          slidesPerView: 1.5,
+          spaceBetween: 10,
+        },
+        601: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        961: {
+          slidesPerView: 1,
+          spaceBetween: 40,
+        },
+        1351: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+      },
+    });
+  }
+});
